bf10972d2f14ca346ae811a84c8c480f332cf6cb
Icaro Gomes <78151906+icarodredd@users.noreply.github.com>
2024-04-03 15:25:29 -0300

    feat: read table

--- TestJBDC/src/main/Main.java
+++ TestJBDC/src/main/Main.java
@@ -1,14 +1,47 @@
 package main;
 
-import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.Database;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Database.getConnection();
-		Database.closeConnection();
+
+		// Obtendo dados SQL
+
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = Database.getConnection().createStatement();
+			rs = st.executeQuery("SELECT * FROM persons");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		try {
+			while (rs.next()) {
+				System.out.println(rs.getInt("id") + ", " + rs.getString("name"));
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				st.close();
+				rs.close();
+				Database.closeConnection();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 
 }
