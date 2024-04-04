a0a136b254c374f03af2d545c2bc98af1e694227
Icaro Gomes <78151906+icarodredd@users.noreply.github.com>
2024-04-04 16:05:06 -0300

    feat: commit and rollback

--- TestJBDC/src/main/Main.java
+++ TestJBDC/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,12 +13,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Obtendo dados SQL
-
+		Connection con = null;
 		Statement st = null;
 		ResultSet rs = null;
 
 		try {
-			st = Database.getConnection().createStatement();
+			con = Database.getConnection();
+			st = con.createStatement();
 			rs = st.executeQuery("SELECT * FROM persons");
 
 		} catch (Exception e) {
@@ -32,15 +35,36 @@ public class Main {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				st.close();
-				rs.close();
-				Database.closeConnection();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			Database.closeStatementAndSet(st, rs);
 
 		}
+
+		// Inserindo dados
+
+		PreparedStatement ps = null;
+		try {
+			con = Database.getConnection();
+			ps = con.prepareStatement("INSERT INTO persons " + "(id, name) " + "VALUES (?, ?)");
+			ps.setInt(1, 3);
+			ps.setString(2, "icaro");
+			// Alterar os dados
+			// Retornando linhas afetadas
+
+			int rowsAffected = ps.executeUpdate();
+			System.out.println(rowsAffected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Database.closeConnection();
+
+		}
+
+		// confirmando as changes na database
+		// con.setAutoCommit(false);
+		// con.commit();
+
+		// voltando as alteraçoes
+		// con.rollback();
 
 	}
 
